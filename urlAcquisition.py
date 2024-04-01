03403d191c74ee101d8fd9f7d94676c23a058e10
Antoni <antonixmajewski@gmail.com>
2024-04-01 21:56:58 +0200

    Extracting data to csv file

--- urlAcquisition.py
+++ urlAcquisition.py
@@ -5,9 +5,7 @@ import csv, re
 
 def fetch_articles_from_api(tag_id, page):
     api_url = f'https://www.unb.com.bd/api/tag-news?tag_id={tag_id}&item={page}'
-
     response = requests.get(api_url)
-
     if response.status_code == 200:
         data = response.json()
         article_urls = []
@@ -17,7 +15,7 @@ def fetch_articles_from_api(tag_id, page):
             article_urls.append(link['href'])
         return article_urls
     else:
-        #print("Failed to fetch articles from the API.")
+        # print("Failed to fetch articles from the API.")
         return []
 
 
@@ -28,9 +26,8 @@ def extract_article_info(url):
         try:
             soup = BeautifulSoup(response.content, 'html.parser')
             # print(soup.prettify())  # Print the HTML content for debugging
-            (publish_date, update_date) = extract_publish_date(soup)
-            meta_location_element = soup.find('li', class_='news-section-bar').find('span', class_='icon fa fa-map-marker')
-            meta_location = meta_location_element.next_sibling.strip()
+            (publish_date, update_date) = extract_dates(soup)
+            meta_location = extract_location(soup)
             title_element = soup.find('title')
             title = title_element.text.strip()
             (html_text, raw_text) = extract_text_data(soup)
@@ -51,10 +48,10 @@ def save_to_csv(articles_data):
             writer.writerow(article_data)
 
 
-def extract_publish_date(soup):
+def extract_dates(soup):
     li_elements = soup.find_all('li')
-    publish_date = ""
-    update_date = ""
+    publish_date = " "
+    update_date = " "
     for li in li_elements:
         if "Publish-" in li.text:
             publish_date = li.text.split("Publish-")[-1].strip()
@@ -63,21 +60,23 @@ def extract_publish_date(soup):
     return publish_date, update_date
 
 
-def extract_update_date(soup):
-    li_elements = soup.find_all('li')
-    for li in li_elements:
-        if "Update-" in li.text:
-            return li.text.split("Update-")[-1].strip()
-    return "-"
+def extract_location(soup):
+    meta_location_element = soup.find('li', class_='news-section-bar').find('span', class_='icon fa fa-map-marker')
+    if meta_location_element:
+        meta_location = meta_location_element.next_sibling.strip()
+        return meta_location
+    else:
+        return " "
 
 
 def extract_text_data(soup):
     target_div = soup.find('div', class_='text')
     if not target_div:
-        return "", ""
+        return " ", " "
     html_text = ''.join(str(p) for p in target_div.find_all('p'))
     raw_text = ' '.join(p.get_text(strip=True) for p in target_div.find_all('p'))
     return html_text, raw_text
+
 
 if __name__ == "__main__":
     tag_id = 54
@@ -87,7 +86,7 @@ if __name__ == "__main__":
     while True:
         current_page = fetch_articles_from_api(tag_id,i)
         if current_page:
-            #print("List of article URLs:"+f'{i}')
+            # print("List of article URLs:"+f'{i}')
             for url in article_urls:
                 print(url)
             article_urls.extend(current_page)
@@ -102,5 +101,3 @@ if __name__ == "__main__":
 
     save_to_csv(articles_data)
 
-    #article_urls is what you need. I'd suggest to nest everything in main in a function.
-    #!!there are over 100 pages of lists of articles. it might take a few seconds to run. Uncomment the print statements if you want to see it.
